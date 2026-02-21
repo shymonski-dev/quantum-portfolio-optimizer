5462f081e3fbc09a4239cb6b00d1d1f9572b3639
worldwidecloud <worldwidecloud@gmail.com>
2026-02-21 18:11:49 +0000

    Security: Scrub hardcoded secrets from manage_jobs utility

--- scripts/manage_jobs.py
+++ scripts/manage_jobs.py
@@ -2,9 +2,15 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 import os
 
 def list_pending_jobs():
-    token = "qX6dC_xUh6g7RDTHgcagMZi4bauvfYobJsXPc0LuKfcD"
-    instance = "crn:v1:bluemix:public:quantum-computing:us-east:a/2e9879e40b034feaa9dd3a6c000673f5:bb92b7f6-559f-4453-b7fd-0f21652093ae::"
+    # Use environment variables for security
+    token = os.environ.get("IBM_QUANTUM_TOKEN")
+    instance = os.environ.get("IBM_CLOUD_INSTANCE")
     
+    if not token:
+        print("Error: IBM_QUANTUM_TOKEN environment variable not set.")
+        return
+    
+    # Instance is optional if default is saved, but recommended for cloud channel
     service = QiskitRuntimeService(channel='ibm_cloud', token=token, instance=instance)
     
     jobs = service.jobs(limit=5)
